630accb7d8786e0c76edeb2a04fbaccdd4576764
mikol <miku2002@wp.pl>
2023-10-20 14:43:04 +0200

    works

---
+++ src/main/java/com/javacode/maven/App.java
@@ -0,0 +1,17 @@
+package com.javacode.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println("u sure about tha?");
+        System.out.println("jafrhuebnfhuew");
+        System.out.println("thats just brutal");
+        System.out.println( "Hello World!" );
+    }
+}
